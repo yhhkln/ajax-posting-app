5134c8665d9180ae4170de3a2df7c1cd073c9e3a
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-06-03 11:53:39 +0800

    add bootstrap to project

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+get "/jquery-1" => "pages#jquery_1"
+get "/jquery-2" => "pages#jquery_2"
+get "/jquery-3" => "pages#jquery_3"
+get "/jquery-4" => "pages#jquery_4"
+get "/jquery-5" => "pages#jquery_5"
+
+root "pages#jquery_1"
