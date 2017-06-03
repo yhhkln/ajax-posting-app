df8a7a5234823d121325c391441ff9bd1411386c
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-06-03 12:56:57 +0800

    ajaxci

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
-get "/jquery-1" => "pages#jquery_1"
-get "/jquery-2" => "pages#jquery_2"
-get "/jquery-3" => "pages#jquery_3"
-get "/jquery-4" => "pages#jquery_4"
-get "/jquery-5" => "pages#jquery_5"
+Rails.application.routes.draw do
+	get "/jquery-1" => "pages#jquery_1"
+	get "/jquery-2" => "pages#jquery_2"
+	get "/jquery-3" => "pages#jquery_3"
+	get "/jquery-4" => "pages#jquery_4"
+	get "/jquery-5" => "pages#jquery_5"
 
-root "pages#jquery_1"
+	root "pages#jquery_1"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
