81623063704b213cfeae0e775ef7359fc74c61cb
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-06-03 18:30:43 +0800

    ajaxcii

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'bootstrap-sass', '~> 3.3.6'
+gem "devise"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -42,6 +43,8 @@ group :development, :test do
 end
 
 group :development do
+  gem 'faker'
+
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.0.5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  root "posts#index"
+  devise_for :users
 	get "/jquery-1" => "pages#jquery_1"
 	get "/jquery-2" => "pages#jquery_2"
 	get "/jquery-3" => "pages#jquery_3"
 	get "/jquery-4" => "pages#jquery_4"
 	get "/jquery-5" => "pages#jquery_5"
-
-	root "pages#jquery_1"
-
+  resources :posts do
+    member do
+      post "like" => "posts#like"
+      post "unlike" => "posts#unlike"
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
